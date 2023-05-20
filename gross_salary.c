c874835f3684c5837dd9f87c426d95ea69e077ef
Gnaneswar484 <133844134+Gnaneswar484@users.noreply.github.com>
2023-05-20 09:01:48 +0530

    Update gross_salary.c

--- gross_salary.c
+++ gross_salary.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int main( ){
+    
+    int basic_salary,allowance,rent,gross_salary;
+    /* Here I declared all the necessary variables 
+    for requirement of calculating gross salary 
+    using the given basic salary */
+    
+    printf("Enter Basic salary ");
+    
+    scanf("%d",&basic_salary);
+    /* In the above line I stored the input basic salary in the 
+    basic_salary variable using & (This operator is used to store the given input into the variable)
+    */
+    
+    allowance=(40*basic_salary)/100;
+    /* Calucating allowance which is 40% of the basic salary*/
+    
+    printf("Allowance = %d\n",allowance);
+    /* Calculating rent which is 20% of the basic salary */
+    
+    rent=(20*basic_salary)/100;
+    
+    printf("Rent = %d\n",rent);
+    /* Calculating the gross_salary which is the combinatin of basic_salry, allowance and rent
+    */
+    gross_salary=basic_salary + allowance + rent;
+    
+    printf("Ramesh gross_salary = %d\n",gross_salary);
+
+}
+/* In the above program I used all the int data types for the 
+round figures If we want exact salary with decimal values we can use float data type */
 
